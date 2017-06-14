bffb7cd13f32225b2d15b0c5402273f88d347cc2
Craig Fisk <craigfisk@picocosmos.net>
2017-06-14 11:53:37 -0700

    Added console.log explanations of relationship between Readable and ReadStream, which inherits from Readable.

--- readable.js
+++ readable.js
@@ -1,6 +1,8 @@
 /* readable.js */
 
 // Create readable stream
+// Usage:
+//   node readable.js
 
 var data = require('./data-sources/sourceData.json'),
   Readable = require('stream').Readable,
@@ -13,3 +15,15 @@ var ReadStream = function() {
 };
 
 util.inherits(ReadStream, Readable);
+
+console.log("Using Object.keys() to examine class methods:");
+
+console.log("Methods available to ReadStream come from the .super_.prototype, which is Readable.prototype.\n");
+
+console.log("Readable:");
+console.log('Object.keys(Readable.prototype)', Object.keys(Readable.prototype));
+
+console.log("ReadStream:");
+console.log('Object.keys(ReadStream.prototype)', Object.keys(ReadStream.prototype));
+console.log('Object.keys(ReadStream)', Object.keys(ReadStream));
+console.log('Object.keys(ReadStream.super_.prototype)', Object.keys(ReadStream.super_.prototype));
