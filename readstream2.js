d59c8b86c9f2be27f1fcfa0390ef5327706815c6
Craig Fisk <craigfisk@picocosmos.net>
2017-06-14 17:55:29 -0700

    Added readstream3.js to require readstream2.js, create a "stream" object from it, then run the stream in "flowing mode" in which data is read from the stream unprompted until it ends.

---
+++ readstream2.js
@@ -0,0 +1,32 @@
+/* readstream2.js */
+
+// Create readable stream
+// Usage:
+//   node
+//   .load readstream2.js
+
+var data = require('./data-sources/sourceData.json'),
+  Readable = require('stream').Readable,
+  util = require('util');
+
+var ReadStream = function() {
+  Readable.call(this, { objectMode: true });
+  this.data = data;
+  this.curIndex = 0;
+};
+
+util.inherits(ReadStream, Readable);
+
+ReadStream.prototype._read = function() {
+  if (this.curIndex === this.data.length)
+    return this.push(null);
+
+  var data = this.data[this.curIndex++];
+  console.log('reading: ' + JSON.stringify(data));
+  this.push(data);
+};
+
+module.exports = ReadStream;
+
+console.log('_read method now will be in ReadStream.prototype and any object constructed from it.');
+console.log('See readstream3.js');
